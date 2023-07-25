512877bb43f73ba30c6af1195f7db80bef9f62b5
Karishma Chawla <kachawla@microsoft.com>
2023-07-25 12:57:23 -0700

    Add context parameter

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ resource "azurerm_redis_cache" "cache" {
   family              = var.family
   sku_name            = var.sku_name
 }
+
+resource "azurerm_redis_cache" "cache2" {
+  name                = var.redis_cache_name
+  location            = var.location
+  resource_group_name = var.context.resourcegroup
+  capacity            = var.capacity
+  family              = var.family
+  sku_name            = var.sku_name
+}
