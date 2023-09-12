641e786452dbb00900658ce7c7647448c78df145
Karishma Chawla <kachawla@microsoft.com>
2023-09-12 12:03:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
 }
 
 resource "azurerm_redis_cache" "cache" {
